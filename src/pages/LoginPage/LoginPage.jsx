ca26ea379d67f95c3c5fc45cb8288b9a5cdcf119
Maksym Drachenko <maks7d@gmail.com>
2025-06-04 15:21:09 +0500

    fix

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -3,7 +3,6 @@ import { LoginForm } from '../../components/LoginForm/LoginForm';
 export default function LoginPage() {
   return (
     <div>
-      {/* <h2>Login</h2> */}
       <LoginForm />
     </div>
   );
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -3,7 +3,6 @@ import { RegisterForm } from '../../components/RegisterForm/RegisterForm';
 export default function RegisterPage() {
   return (
     <div>
-      {/* <h2>Registration</h2> */}
       <RegisterForm />
     </div>
   );
